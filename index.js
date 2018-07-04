bc7251be4798d9dd70613eca5ca2b0690ff57f9a
Ira Miller <public@iramiller.com>
2018-07-04 11:37:54 -0500

    standardized repos

---
+++ index.js
@@ -0,0 +1,67 @@
+const bitbucketjs = require('bitbucketjs')
+const { getName } = require('guld-user')
+const { getPass, getHostName } = require('guld-git-host').util
+const HOST = 'bitbucket'
+var client
+
+async function getClient (user) {
+  user = user || await getName()
+  var pass = await getPass(user, HOST)
+  return new bitbucketjs({ // eslint-disable-line new-cap
+    username: pass.login,
+    password: pass.password
+  })
+}
+
+function parseRepo (repo) {
+  var privacy
+  var mainbranch
+  if (repo.is_private) privacy = 'private'
+  else privacy = 'public'
+  if (repo.mainbranch && repo.mainbranch.name) mainbranch = repo.mainbranch.name
+  else mainbranch = repo.owner.name
+  return {
+    name: repo.name,
+    privacy: privacy,
+    owner: repo.owner.name,
+    mainbranch: mainbranch
+  }
+}
+
+async function createRepo (rname, user, privacy = 'public', options = {}) {
+  // validate required field(s)
+  if (typeof rname !== 'string' || rname.length === 0) throw new Error('Name is required to create repo.')
+  user = user || await getName()
+  var hostuser = await getHostName(user, HOST) || user
+  // set privacy
+  if (!options.hasOwnProperty('is_private')) {
+    if (privacy === 'public') options.is_private = false // eslint-disable-line camelcase
+    else options.is_private = true // eslint-disable-line camelcase
+  }
+  // assume git
+  if (!options.hasOwnProperty('scm')) options.scm = 'git'
+  client = client || await getClient(user)
+  return parseRepo(await client.repo.create(`${hostuser}/${rname}`, options))
+}
+
+async function listRepos (user) {
+  user = user || await getName()
+  var hostuser = await getHostName(user, HOST) || user
+  client = client || await getClient(user)
+  var ls = await client.repo.forOwner(hostuser)
+  return ls.values.map(parseRepo)
+}
+
+async function deleteRepo (rname, user) {
+  user = user || await getName()
+  var hostuser = await getHostName(user, HOST) || user
+  client = client || await getClient(user)
+  return await client.repo.delete(`${hostuser}/${rname}`)
+}
+
+module.exports = {
+  getClient: getClient,
+  createRepo: createRepo,
+  listRepos: listRepos,
+  deleteRepo: deleteRepo
+}
---
+++ test.js
@@ -0,0 +1,28 @@
+/* eslint-env node, mocha */
+const assert = require('chai').assert
+const { getName } = require('guld-user')
+const { getClient, listRepos, createRepo, deleteRepo } = require('./index.js')
+
+describe('bitbucket', function () {
+  before(async function () {
+    this.user = await getName()
+  })
+  it('getClient', async function () {
+    var client = await getClient(this.user)
+    assert.exists(client)
+  })
+  it('listRepos', async function () {
+    this.repos = await listRepos(this.user)
+    assert.exists(this.repos)
+    assert.isTrue(this.repos.length > 0)
+  }).timeout(5000)
+  it('createRepo', async function () {
+    this.repo = await createRepo('guld-repo-for-test')
+    assert.exists(this.repo)
+    assert.exists(this.repo.name)
+    assert.equal(this.repo.name, 'guld-repo-for-test')
+  }).timeout(5000)
+  it('deleteRepo', async function () {
+    this.repo = await deleteRepo('guld-repo-for-test')
+  }).timeout(5000)
+})
